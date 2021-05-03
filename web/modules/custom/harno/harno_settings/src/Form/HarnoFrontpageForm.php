a816ecdc435a6b93f46a13821933ae3ec3f58819
Egon Tinno <egon.tinno@twn.ee>
2021-05-03 11:49:08 +0300

    HV-321 | Admin: Bännerite haldamine

--- web/modules/custom/harno/harno_settings/src/Form/HarnoFrontpageForm.php
+++ web/modules/custom/harno/harno_settings/src/Form/HarnoFrontpageForm.php
@@ -9,9 +9,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\file\Entity\File;
 
 /**
- * Class HarnoSettingsForm.
+ * Class HarnoFrontpageForm.
  */
-class HarnoSettingsForm extends ConfigFormBase {
+class HarnoFrontpageForm extends ConfigFormBase {
 
   /**
    * HarnoSettingsForm constructor.
@@ -38,12 +38,11 @@ class HarnoSettingsForm extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'system.site',
       'harno_settings.settings',
     ];
   }
   public function getFormId() {
-    return 'harno_settings_form';
+    return 'harno_frontpage_form';
   }
 
   /**
@@ -59,78 +58,46 @@ class HarnoSettingsForm extends ConfigFormBase {
       '#type' => 'vertical_tabs',
     ];
 
-    ##################################################### Haridusasutuse üldinfo #################################################
+    ##################################################### Bänneri/tausatapildi seadistus #################################################
     $form['general'] = [
       '#type' => 'details',
-      '#title' => 'Haridusasutuse üldinfo',
-      '#description' => 'Üldkontaktide sisestamise ja muutmise andmeplokk.',
+      '#title' => 'Bänneri/tausatapildi seadistus',
+      '#description' => 'Siin saab valida, kas avalehel kuvatakse: avalehe taustapilti, bännerit piltidega või bännerit kastidega.',
       '#group' => 'tabs',
     ];
 
-    $form['general']['site_name'] = [
-      '#type' => 'textfield',
-      '#title' => 'Haridusasutuse nimi',
+    $form['general']['frontpage_background_type'] = [
+      '#type' => 'select',
+      '#title' => 'Kas avalehel kuvatakse: ',
+      '#options' => [
+        1 => 'Avalehe taustapilti',
+        2 => 'Bännerit piltidega',
+        3 => 'Bännerit kastidega',
+      ],
+      '#default_value' =>  $config->get('general.frontpage_background_type') ? $config->get('general.frontpage_background_type') : 1,
       '#required' => TRUE,
-      '#description' => 'Kuvatakse päises, jaluses ja veebilehitseja vahekaardil',
-      '#default_value' =>  $config_site->get('name'),
     ];
-    $form['general']['slogan'] = [
-      '#type' => 'textfield',
-      '#title' => 'Haridusasutuse moto',
-      '#description' => 'Kuvatakse päises',
-      '#default_value' =>  $config_site->get('slogan'),
-    ];
-    $form['general']['logo_default'] = [
+
+    $form['general']['frontpage_background_default'] = [
       '#type' => 'value',
-      '#value' => $config->get('general.logo'),
+      '#value' => $config->get('general.frontpage_background'),
     ];
-    $form['general']['logo_upload'] = [
+    $form['general']['frontpage_background_upload'] = [
       '#type' => 'managed_file',
-      '#title' => 'Haridusasutuse logo',
-      '#description' => 'Kuvatakse päises. Lubatud vormingud on .jpg, .jpeg, .png või .svg. Maksimaalne lubatud laius on 416px ja maksimaalne kõrgus on 224px.',
-      '#default_value' =>  [$config->get('general.logo')],
-      '#upload_location' => 'public://logo',
+      '#title' => 'Avalehe taustapilt',
+      '#description' => 'Kuvatakse avalehe päises. Lubatud vormingud on .jpg, .jpeg või .png. Minimaalne vajalik laius on 2800px ja minimaalne kõrgus on 800px.',
+      '#default_value' =>  [$config->get('general.frontpage_background')],
+      '#upload_location' => 'public://frontpage_background',
       '#upload_validators' => [
-        'file_validate_image_resolution' => ['416x224', '0'],
+        'file_validate_image_resolution' => ['0', '2800x800'],
         'file_validate_extensions' => [
-          'jpg jpeg png svg'
+          'jpg jpeg png'
         ],
       ],
     ];
-    $form['general']['favicon_default'] = [
-      '#type' => 'value',
-      '#value' => $config->get('general.favicon'),
-    ];
-    $form['general']['favicon_upload'] = [
-      '#type' => 'managed_file',
-      '#title' => 'Haridusasutuse veebilehe tunnusikoon (favicon)',
-      '#description' => 'Kuvatakse veebilehitseja vahekaardil. Lubatud vorming on .ico',
-      '#default_value' =>  [$config->get('general.favicon')],
-      '#upload_location' => 'public://',
-      '#upload_validators' => [
-        'file_validate_extensions' => [
-          'ico',
-        ],
-      ],
-    ];
-    $form['general']['address'] = [
-      '#type' => 'textfield',
-      '#title' => 'Haridusasutuse üldkontakti aadress',
-      '#description' => 'Kuvatakse jaluses',
-      '#default_value' =>  $config->get('general.address'),
-    ];
-    $form['general']['phone'] = [
-      '#type' => 'textfield',
-      '#title' => 'Haridusasutuse üldkontakti telefoni number',
-      '#description' => 'Kuvatakse jaluses',
-      '#default_value' =>  $config->get('general.phone'),
-    ];
-    $form['general']['email'] = [
-      '#type' => 'email',
-      '#title' => 'Haridusasutuse üldkontakti e-posti aadress',
-      '#description' => 'Kuvatakse jaluses',
-      '#default_value' =>  $config->get('general.email'),
-    ];
+
+
+
     ##################################################### Avalehe kiirlingid #################################################
     $form['frontpage_quick_links'] = [
       '#type' => 'details',
@@ -496,74 +463,6 @@ class HarnoSettingsForm extends ConfigFormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
 
-    $frontpage_quick_links = $form_state->getValue('fp_quick_links_table');
-    foreach ($frontpage_quick_links as $id => $item) {
-      $j = $id + 1;
-      if (!empty($item['link_name']) AND empty($item['link_entity']) AND empty($item['link_url'])) {
-        $message = 'Palun täida avalehe kiirlingid real '.$j. ', kas väli "sisemine link" või "väline veebilink".';
-        $form_state->setErrorByName('fp_quick_links_table][' . $id . '][link_entity', $message);
-        $form_state->setErrorByName('fp_quick_links_table][' . $id . '][link_url', $message);
-      }
-      if (!empty($item['link_entity']) AND empty($item['link_name'])) {
-        $form_state->setErrorByName('fp_quick_links_table]['.$id.'][link_name', $this->t('@name field is required.', ['@name' => '"veebilingi väljakuvatav nimi" real ' . $j]));
-      }
-      if (!empty($item['link_url']) AND empty($item['link_name'])) {
-        $form_state->setErrorByName('fp_quick_links_table]['.$id.'][link_name', $this->t('@name field is required.', ['@name' => '"veebilingi väljakuvatav nimi" real ' . $j]));
-      }
-    }
-
-    $socialmedia_links = $form_state->getValue('socialmedia_table');
-    foreach ($socialmedia_links as $id => $item) {
-      $j = $id + 1;
-      if (!empty($item['link_icon'])) {
-        if (empty($item['link_name'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_name', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia lingi nimetus" real ' . $j]));
-        }
-        if (empty($item['link_url'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_url', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia konto veebilink" real ' . $j]));
-        }
-      }
-
-      if (!empty($item['link_name'])) {
-        if (empty($item['link_icon'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_icon', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia lingi ikoon" real ' . $j]));
-        }
-        if (empty($item['link_url'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_url', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia konto veebilink" real ' . $j]));
-        }
-      }
-      if (!empty($item['link_url'])) {
-        if (empty($item['link_icon'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_icon', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia lingi ikoon" real ' . $j]));
-        }
-        if (empty($item['link_name'])) {
-          $form_state->setErrorByName('socialmedia_table][' . $id . '][link_name', $this->t('@name field is required.', ['@name' => '"sotsiaalmeedia lingi nimetus" real ' . $j]));
-        }
-      }
-    }
-
-    $footer_quick_links = $form_state->getValue('quick_links_table');
-    foreach ($footer_quick_links as $id => $item) {
-      $j = $id + 1;
-      if (!empty($item['link_name']) AND empty($item['link_entity']) AND empty($item['link_url'])) {
-        $message = 'Palun täida jaluse kiirlingid real '.$j. ', kas väli "sisemine link" või "väline veebilink".';
-        $form_state->setErrorByName('quick_links_table][' . $id . '][link_entity', $message);
-        $form_state->setErrorByName('quick_links_table][' . $id . '][link_url', $message);
-      }
-      if (!empty($item['link_entity']) AND empty($item['link_name'])) {
-        $form_state->setErrorByName('quick_links_table]['.$id.'][link_name', $this->t('@name field is required.', ['@name' => '"veebilingi väljakuvatav nimi" real ' . $j]));
-      }
-      if (!empty($item['link_url']) AND empty($item['link_name'])) {
-        $form_state->setErrorByName('quick_links_table]['.$id.'][link_name', $this->t('@name field is required.', ['@name' => '"veebilingi väljakuvatav nimi" real ' . $j]));
-      }
-    }
-
-    if (!empty($form_state->getValue('free_text_name')) AND empty($form_state->getValue('free_text_body'))) {
-      $form_state->setErrorByName('free_text_body', $this->t('@name field is required.', ['@name' => '"vabatekstiala sisu"']));
-    }
-    if (!empty($form_state->getValue('free_text_body')) AND empty($form_state->getValue('free_text_name'))) {
-      $form_state->setErrorByName('free_text_name', $this->t('@name field is required.', ['@name' => '"vabatekstiala pealkiri"']));
-    }
   }
 
   /**
@@ -571,109 +470,18 @@ class HarnoSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-    ################################################### Logo save ######################################
-    if ($form_state->getValue('logo_upload')) {
-      $logo_upload = $form_state->getValue('logo_upload')[0];
+    ################################################### Frontpage background save ######################################
+    if ($form_state->getValue('frontpage_background_upload')) {
+      $background_upload = $form_state->getValue('frontpage_background_upload')[0];
     }
     else {
-      $logo_upload = 0;
+      $background_upload = 0;
     }
-    $this->fileUploadHandle($logo_upload, $form_state->getValue('logo_default'), 'general.logo');
-
-    ################################################### Favicon save ######################################
-    if ($form_state->getValue('favicon_upload')) {
-      $favicon_upload = $form_state->getValue('favicon_upload')[0];
-    }
-    else {
-      $favicon_upload = 0;
-    }
-    $this->fileUploadHandle($favicon_upload, $form_state->getValue('favicon_default'), 'general.favicon');
-
+    $this->fileUploadHandle($background_upload, $form_state->getValue('frontpage_background_default'), 'general.frontpage_background');
     ################################################### Other general settings save ######################################
     $this->config('harno_settings.settings')
-      ->set('general.address', $form_state->getValue('address'))
-      ->set('general.phone', $form_state->getValue('phone'))
-      ->set('general.email', $form_state->getValue('email'))
-      ->set('footer_free_text_area.name', $form_state->getValue('free_text_name'))
-      ->set('footer_free_text_area.body', $form_state->getValue('free_text_body'))
-      ->set('footer_copyright.name', $form_state->getValue('footer_copyright_name'))
-      ->set('automatic_generation_academic_year.on', $form_state->getValue('automatic_generation_academic_year_on'))
-      ->set('automatic_generation_academic_year.date', $form_state->getValue('automatic_generation_academic_year_date'))
-      ->set('news_our_story.name', $form_state->getValue('news_our_story_name'))
+      ->set('general.frontpage_background_type', $form_state->getValue('frontpage_background_type'))
       ->save();
-    ################################################### Frontpage quick links settings save ######################################
-
-    $frontpage_quick_links = $form_state->getValue('fp_quick_links_table');
-
-    $keys = array_column($frontpage_quick_links, 'link_weight');
-    array_multisort($keys, SORT_ASC, $frontpage_quick_links);
-
-    foreach ($frontpage_quick_links as $id => $item) {
-      $j = $id + 1;
-
-      $this->config('harno_settings.settings')
-        ->set('frontpage_quick_links.link_name_'. $j, $item['link_name'])
-        ->set('frontpage_quick_links.link_entity_'. $j, $item['link_entity'])
-        ->set('frontpage_quick_links.link_url_'. $j, $item['link_url'])
-        ->set('frontpage_quick_links.link_weight_'. $j, $item['link_weight'])
-        ->save();
-    }
-
-    ################################################### System site settings save ######################################
-
-    $this->config('system.site')
-      ->set('name', $form_state->getValue('site_name'))
-      ->set('slogan', $form_state->getValue('slogan'))
-      ->save();
-
-    ################################################### Important contacts settings save ######################################
-    $footer_important_contacts = $form_state->getValue('contacts_table');
-
-    $keys = array_column($footer_important_contacts, 'weight');
-    array_multisort($keys, SORT_ASC, $footer_important_contacts);
-
-    foreach ($footer_important_contacts as $id => $item) {
-      $j = $id + 1;
-      $this->config('harno_settings.settings')
-        ->set('important_contacts.name_'. $j, $item['name'])
-        ->set('important_contacts.body_'. $j, $item['body'])
-        ->set('important_contacts.weight_'. $j, $item['weight'])
-        ->save();
-    }
-    ################################################### Footer socialmedia settings save ######################################
-
-    $socialmedia_links = $form_state->getValue('socialmedia_table');
-
-    $keys = array_column($socialmedia_links, 'link_weight');
-    array_multisort($keys, SORT_ASC, $socialmedia_links);
-
-    foreach ($socialmedia_links as $id => $item) {
-      $j = $id + 1;
-      $this->config('harno_settings.settings')
-        ->set('footer_socialmedia_links.link_icon_'. $j, $item['link_icon'])
-        ->set('footer_socialmedia_links.link_name_'. $j, $item['link_name'])
-        ->set('footer_socialmedia_links.link_url_'. $j, $item['link_url'])
-        ->set('footer_socialmedia_links.link_weight_'. $j, $item['link_weight'])
-        ->save();
-    }
-    ################################################### Footer quick links settings save ######################################
-
-    $footer_quick_links = $form_state->getValue('quick_links_table');
-
-    $keys = array_column($footer_quick_links, 'link_weight');
-    array_multisort($keys, SORT_ASC, $footer_quick_links);
-
-    foreach ($footer_quick_links as $id => $item) {
-      $j = $id + 1;
-
-      $this->config('harno_settings.settings')
-        ->set('footer_quick_links.link_name_'. $j, $item['link_name'])
-        ->set('footer_quick_links.link_entity_'. $j, $item['link_entity'])
-        ->set('footer_quick_links.link_url_'. $j, $item['link_url'])
-        ->set('footer_quick_links.link_weight_'. $j, $item['link_weight'])
-        ->save();
-    }
-
   }
 
   /**
