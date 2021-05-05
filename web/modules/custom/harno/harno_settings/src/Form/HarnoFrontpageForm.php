617cce09bbe9296ef21600369919a6593de814fa
Egon Tinno <egon.tinno@twn.ee>
2021-05-05 17:41:20 +0300

    HV-530 | Admin: Bänner kastidega sisestamine/muutmine

--- web/modules/custom/harno/harno_settings/src/Form/HarnoFrontpageForm.php
+++ web/modules/custom/harno/harno_settings/src/Form/HarnoFrontpageForm.php
@@ -204,8 +204,143 @@ class HarnoFrontpageForm extends ConfigFormBase {
         '#attributes' => ['class' => ['table-sort-weight']],
       ];
     }
+    ##################################################### Bänner kastidega #################################################
+    $form['banner_boxes_images'] = [
+      '#type' => 'details',
+      '#title' => 'Bänner kastidega',
+      '#description' => 'Kuvatakse avalehe päises. Bänneri faili lubatud vormingud on .jpg, .jpeg või .png, minimaalne vajalik laius on 2800px ja minimaalne kõrgus on 800px.
+      Ikooni nimetusi vaata <a href="/ikoonid" target="_blank">siit</a>.',
+      '#group' => 'tabs',
+    ];
+    $form['banner_boxes_images']['fp_banner_boxes_images_table'] = [
+      '#type' => 'table',
+      '#caption' => 'Pildid',
+      '#header' => [
+        '',
+        'Bänneri taustapilt',
+        'Kaal'
+      ],
+      // TableDrag: Each array value is a list of callback arguments for
+      // drupal_add_tabledrag(). The #id of the table is automatically
+      // prepended; if there is none, an HTML ID is auto-generated.
+      '#tabledrag' => [
+        [
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'table-sort-weight',
+        ],
+      ],
+    ];
+    for ($i = 0; $i < 5; $i++) {
+      $j = $i + 1;
+      // TableDrag: Mark the table row as draggable.
+      $form['banner_boxes_images']['fp_banner_boxes_images_table'][$i]['#attributes']['class'][] = 'draggable';
+      // TableDrag: Sort the table row according to its existing/configured
+      // weight.
+      $form['banner_boxes_images']['fp_banner_boxes_images_table'][$i]['#weight'] = $config->get('banner_boxes_images.link_weight_' . $j);
+
+      $form['banner_boxes_images']['fp_banner_boxes_images_table'][$i]['banner_boxes_image_default'] = [
+        '#type' => 'value',
+        '#value' => $config->get('banner_boxes_images.image_'. $j),
+      ];
+      $form['banner_boxes_images']['fp_banner_boxes_images_table'][$i]['banner_boxes_image_upload'] = [
+        '#type' => 'managed_file',
+        '#title' => 'Bänneri taustapilt ' . $j,
+        '#title_display' => 'invisible',
+        '#default_value' =>  [$config->get('banner_boxes_images.image_'. $j)],
+        '#upload_location' => 'public://frontpage_banner_images',
+        '#upload_validators' => [
+          'file_validate_image_resolution' => ['0', '2800x800'],
+          'file_validate_extensions' => [
+            'jpg jpeg png'
+          ],
+        ],
+      ];
+
+      // TableDrag: Weight column element.
+      $form['banner_boxes_images']['fp_banner_boxes_images_table'][$i]['link_weight'] = [
+        '#type' => 'weight',
+        '#title' => $this->t('Weight for @title', ['@title' => 'link ' . $j]),
+        '#title_display' => 'invisible',
+        '#default_value' => $config->get('banner_boxes_images.link_weight_' . $j),
+        // Classify the weight element for #tabledrag.
+        '#attributes' => ['class' => ['table-sort-weight']],
+      ];
+    }
+
+    ##################################################### Bänner kastidega tekstid #################################################
+
+    $form['banner_boxes_images']['fp_banner_boxes_texts_table'] = [
+      '#type' => 'table',
+      '#caption' => 'Kastid',
+      '#sticky' => TRUE,
+      '#header' => [
+        'Kasti pealkiri',
+        'Kasti ikooni nimi',
+        'Kasti sisemine link',
+        'Kaal'
+      ],
+      // TableDrag: Each array value is a list of callback arguments for
+      // drupal_add_tabledrag(). The #id of the table is automatically
+      // prepended; if there is none, an HTML ID is auto-generated.
+      '#tabledrag' => [
+        [
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'table-sort-weight',
+        ],
+      ],
+    ];
+    for ($i = 0; $i < 15; $i++) {
+      $j = $i + 1;
+      // TableDrag: Mark the table row as draggable.
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['#attributes']['class'][] = 'draggable';
+      // TableDrag: Sort the table row according to its existing/configured
+      // weight.
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['#weight'] = $config->get('banner_boxes_text.link_weight_' . $j);
 
 
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['title'] = [
+        '#type' => 'textfield',
+        '#title' => 'Kasti pealkiri ' . $j,
+        '#title_display' => 'invisible',
+        '#size' => 30,
+        '#maxlength' => 35,
+        '#default_value' => $config->get('banner_boxes_text.title_' . $j),
+      ];
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['icon'] = [
+        '#type' => 'textfield',
+        '#title' => 'Kasti ikoon ' . $j,
+        '#title_display' => 'invisible',
+        '#size' => 15,
+        '#default_value' => $config->get('banner_boxes_text.icon_' . $j),
+      ];
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['link_entity'] = [
+        '#type' => 'entity_autocomplete',
+        '#size' => 40,
+        '#title' => 'Sisemine link ' . $j,
+        '#title_display' => 'invisible',
+        '#target_type' => 'node',
+        '#selection_handler' => 'default', // Optional. The default selection handler is pre-populated to 'default'.
+        #'#selection_settings' => [
+        #  'target_bundles' => ['article', 'page'],
+        #],
+      ];
+      if (!empty($config->get('banner_boxes_text.link_entity_' . $j))) {
+        $node = \Drupal::entityTypeManager()->getStorage('node')->load($config->get('banner_boxes_text.link_entity_' . $j));
+        $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['link_entity']['#default_value'] = $node;
+      }
+
+      // TableDrag: Weight column element.
+      $form['banner_boxes_images']['fp_banner_boxes_texts_table'][$i]['link_weight'] = [
+        '#type' => 'weight',
+        '#title' => $this->t('Weight for @title', ['@title' => 'link ' . $j]),
+        '#title_display' => 'invisible',
+        '#default_value' => $config->get('banner_boxes_text.link_weight_' . $j),
+        // Classify the weight element for #tabledrag.
+        '#attributes' => ['class' => ['table-sort-weight']],
+      ];
+    }
     return $form;
   }
 
@@ -214,41 +349,96 @@ class HarnoFrontpageForm extends ConfigFormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
-
-    $background_type = $form_state->getValue('background_type');
-    if ($background_type == 1 AND empty($form_state->getValue('background_upload'))) {
-      $message = '"Avalehe taustapilt" on kohustuslik, kui "Avalehel kuvatakse" väljas on valitud "Avalehe taustapilti".';
-      $form_state->setErrorByName('background_upload', $message);
-    }
+    $banner_images_first_row = $banner_boxes_images_first_row = $banner_boxes_texts_first_row = FALSE;
     $banner_images = $form_state->getValue('fp_banner_images_table');
     foreach ($banner_images as $id => $item) {
       $j = $id + 1;
-      if (!empty($item['banner_image_upload']) AND empty($item['banner_text'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' väli "Bänneri tekst".';
+      if (!empty($item['banner_image_upload'])) {
+        $banner_images_first_row = TRUE;
+      }
+      if (!empty($item['banner_image_upload']) and empty($item['banner_text'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' väli "Bänneri tekst".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][banner_text', $message);
       }
-      if (!empty($item['banner_image_upload']) AND empty($item['link_name'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' väli "Lingi kuvatav tekst".';
+      if (!empty($item['banner_image_upload']) and empty($item['link_name'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' väli "Lingi kuvatav tekst".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_name', $message);
       }
-      if (!empty($item['banner_image_upload']) AND empty($item['link_entity']) AND empty($item['link_url'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' kas väli "Sisemine link" või "Väline veebilink".';
+      if (!empty($item['banner_image_upload']) and empty($item['link_entity']) and empty($item['link_url'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' kas väli "Sisemine link" või "Väline veebilink".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_entity', $message);
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_url', $message);
       }
-      if (!empty($item['banner_text']) AND empty($item['banner_image_upload'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' väli "Bänneri taustapilt".';
+      if (!empty($item['banner_text']) and empty($item['banner_image_upload'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' väli "Bänneri taustapilt".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][banner_image_upload', $message);
       }
-      if (!empty($item['banner_text']) AND empty($item['link_name'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' väli "Lingi kuvatav tekst".';
+      if (!empty($item['banner_text']) and empty($item['link_name'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' väli "Lingi kuvatav tekst".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_name', $message);
       }
-      if (!empty($item['banner_text']) AND empty($item['link_entity']) AND empty($item['link_url'])) {
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' väli "Lingi kuvatav tekst".';
-        $message = 'Palun täida sakis "Bänner piltidega" real '.$j. ' kas väli "Sisemine link" või "Väline veebilink".';
+      if (!empty($item['banner_text']) and empty($item['link_entity']) and empty($item['link_url'])) {
+        $message = 'Palun täida sakis "Bänner piltidega" real ' . $j . ' kas väli "Sisemine link" või "Väline veebilink".';
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_entity', $message);
         $form_state->setErrorByName('fp_banner_images_table][' . $id . '][link_url', $message);
+      }
+    }
+    $banner_boxes_images = $form_state->getValue('fp_banner_boxes_images_table');
+    foreach ($banner_boxes_images as $id => $item) {
+      if (!empty($item['banner_image_upload'])) {
+        $banner_boxes_images_first_row = TRUE;
+      }
+    }
+    $banner_boxes_texts = $form_state->getValue('fp_banner_boxes_texts_table');
+    foreach ($banner_boxes_texts as $id => $item) {
+      $j = $id + 1;
+      if (!empty($item['title'])) {
+        $banner_boxes_texts_first_row = TRUE;
+      }
+
+      if (!empty($item['title']) and empty($item['icon'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti ikooni nimi".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][icon', $message);
+      }
+      if (!empty($item['title']) and empty($item['link_entity'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti sisemine link".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][link_entity', $message);
+      }
+      if (!empty($item['icon']) and empty($item['title'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti pealkiri".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][title', $message);
+      }
+      if (!empty($item['icon']) and empty($item['link_entity'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti sisemine link".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][link_entity', $message);
+      }
+      if (!empty($item['link_entity']) and empty($item['title'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti pealkiri".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][title', $message);
+      }
+      if (!empty($item['link_entity']) and empty($item['icon'])) {
+        $message = 'Palun täida sakis "Bänner kastidega" real ' . $j . ' väli "Kasti ikooni nimi".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][' . $id . '][icon', $message);
+      }
+    }
+
+    $background_type = $form_state->getValue('background_type');
+    if ($background_type == 1 and empty($form_state->getValue('background_upload'))) {
+      $message = '"Avalehe taustapilt" on kohustuslik, kui "Avalehel kuvatakse" väljas on valitud "Avalehe taustapilti".';
+      $form_state->setErrorByName('background_upload', $message);
+    }
+    elseif ($background_type == 2 and !$banner_images_first_row) {
+      $message = '"Bänneri taustapilt" sakis "Bänner piltidega" on kohustuslik, kui "Avalehel kuvatakse" väljas on valitud "Bännerit piltidega".';
+      $form_state->setErrorByName('fp_banner_images_table][0][banner_image_upload', $message);
+    }
+    elseif ($background_type == 3) {
+      if (!$banner_boxes_images_first_row) {
+        $message = '"Bänneri taustapilt" sakis "Bänner kastidega" on kohustuslik, kui "Avalehel kuvatakse" väljas on valitud "Bännerit kastidega".';
+        $form_state->setErrorByName('fp_banner_boxes_images_table][0][banner_image_upload', $message);
+      }
+      if (!$banner_boxes_texts_first_row) {
+        $message = '"Kasti pealkiri" sakis "Bänner kastidega" on kohustuslik, kui "Avalehel kuvatakse" väljas on valitud "Bännerit kastidega".';
+        $form_state->setErrorByName('fp_banner_boxes_texts_table][0][title', $message);
       }
     }
   }
@@ -270,7 +460,7 @@ class HarnoFrontpageForm extends ConfigFormBase {
     $this->config('harno_settings.frontpage')
       ->set('general.background_type', $form_state->getValue('background_type'))
       ->save();
-    ################################################### Frontpage quick links settings save ######################################
+    ################################################### Bänner piltidega settings save ######################################
 
     $banner_images = $form_state->getValue('fp_banner_images_table');
 
@@ -295,6 +485,44 @@ class HarnoFrontpageForm extends ConfigFormBase {
         ->set('banner_images.link_weight_'. $j, $item['link_weight'])
         ->save();
     }
+
+    ################################################### Bänner kastidega settings save ######################################
+
+    $banner_boxes_images = $form_state->getValue('fp_banner_boxes_images_table');
+
+    $keys = array_column($banner_boxes_images, 'link_weight');
+    array_multisort($keys, SORT_ASC, $banner_boxes_images);
+
+    foreach ($banner_boxes_images as $id => $item) {
+      $j = $id + 1;
+      if ($item['banner_boxes_image_upload']) {
+        $background_upload = $item['banner_boxes_image_upload'][0];
+      }
+      else {
+        $background_upload = 0;
+      }
+      $this->fileUploadHandle($background_upload, $item['banner_boxes_image_default'], 'banner_boxes_images.image_'. $j);
+
+      $this->config('harno_settings.frontpage')
+        ->set('banner_boxes_images.link_weight_'. $j, $item['link_weight'])
+        ->save();
+    }
+
+    $banner_boxes_texts = $form_state->getValue('fp_banner_boxes_texts_table');
+
+    $keys = array_column($banner_boxes_texts, 'link_weight');
+    array_multisort($keys, SORT_ASC, $banner_boxes_texts);
+
+    foreach ($banner_boxes_texts as $id => $item) {
+      $j = $id + 1;
+      $this->config('harno_settings.frontpage')
+        ->set('banner_boxes_text.title_'. $j, $item['title'])
+        ->set('banner_boxes_text.icon_'. $j, $item['icon'])
+        ->set('banner_boxes_text.link_entity_'. $j, $item['link_entity'])
+        ->set('banner_boxes_text.link_weight_'. $j, $item['link_weight'])
+        ->save();
+    }
+
   }
 
   /**
