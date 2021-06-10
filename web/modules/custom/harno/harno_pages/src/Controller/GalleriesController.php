aaa5999296997953d36e3173208513c794308b91
Madis Loik <madis.loik@twn.ee>
2021-06-10 20:49:57 +0300

    URL táiendus themes

--- web/modules/custom/harno/harno_pages/src/Controller/GalleriesController.php
+++ web/modules/custom/harno/harno_pages/src/Controller/GalleriesController.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Controller for controllingg Galleries.
+ */
 namespace Drupal\harno_pages\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
@@ -162,23 +164,28 @@ class GalleriesController extends ControllerBase {
       $term->{'#start_year'} = $start;
       $terms_by_year[$key] = $term;
     }
-    usort($terms_by_year, function ($a, $b) {
-      if (!empty($a->{'#start_year'}) && !empty($b->{'#start_year'})) {
-        $aweight = strtotime($a->{'#start_year'});
-        $bweight = strtotime($b->{'#start_year'});
-        return $bweight - $aweight;
-      }
-    });
+    if (!empty($terms_by_year)) {
+      usort($terms_by_year, function ($a, $b) {
+        if (!empty($a->{'#start_year'}) && !empty($b->{'#start_year'})) {
+          $aweight = strtotime($a->{'#start_year'});
+          $bweight = strtotime($b->{'#start_year'});
+          return $bweight - $aweight;
+        }
+      });
+    }
+
     $active_terms = [];
     $count = 0;
-    foreach ($terms_by_year as $term) {
+    if (!empty($terms_by_year)) {
+      foreach ($terms_by_year as $term) {
 
-      if ($count > 4) {
-        $active_terms['older'] = t('Older galleries');
-        break;
+        if ($count > 4) {
+          $active_terms['older'] = t('Older galleries');
+          break;
+        }
+        $active_terms[$term->getName()] = $term->getName();
+        $count++;
       }
-      $active_terms[$term->getName()] = $term->getName();
-      $count++;
     }
     if (!empty($active_terms)) {
       return $active_terms;
